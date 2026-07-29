7649c45bea99a5760977b7b62970f79880528a7d
Axel leandro Coria Venegas <axelleandrocoriavenegas@192.168.1.3>
2026-07-28 20:01:12 -0600

    cambios

---
+++ script.js
@@ -0,0 +1,9 @@
+const opciones = document.getElementById("opciones");
+const chi = document.getElementById("chi");
+chi.addEventListener("click", function(){
+    if(opciones.value == 1){
+        chi.innerHTML = "<h2>Hola mi amor, tiquita </h2><p>Chabia Que dirias que chi por eso te hice esta pagina bebecita linda ❤️</p>";
+    }
+
+});
+
